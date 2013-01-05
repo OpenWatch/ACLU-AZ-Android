52c8163313d8e3bf379cf58eeb4e50ab3408b7d4
David Brodsky <davidpbrodsky@gmail.com>
2013-01-05 14:10:37 -0800

    remove actionbar text and icon where appropriate

--- src/net/openwatch/acluaz/MainActivity.java
+++ src/net/openwatch/acluaz/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends SherlockActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.getSupportActionBar().setDisplayShowHomeEnabled(false);
 	}
 
 	@Override
